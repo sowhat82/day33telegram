7d5266afb5dbf1ca2415d132056652b7d0f5723a
Alvin <alvincheong@u.nus.edu>
2021-01-07 15:13:48 +0800

    latest

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ global.env = secureEnv({secret:'mySecretPassword'})
 const fs = require ('fs/promises')
 const withquery = require('with-query').default
 const fetch = require('node-fetch')
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
 
 //create a bot
 const bot = new Telegraf(global.env.TELEGRAM_TOKEN)
@@ -16,20 +17,25 @@ bot.start(ctx => {
 
     // 2 ways to display a welcome picture, either by URL, or by a local file
     // ctx.replyWithPhoto(photoURL, {caption: 'Hello!!!'})
-    fs.readFile(__dirname + '/images/img-file.jpg').then((buffer) => {
-        ctx.replyWithPhoto({ source: buffer });
-      });
+    // fs.readFile(__dirname + '/images/img-file.jpg').then((buffer) => {
+    //     ctx.replyWithPhoto({ source: buffer });
+    //   });
 
     ctx.reply('Welcome to my Bot')
 })
 
-bot.hears('hi', ctx => ctx.reply ('hi!'))
+bot.hears('hi', ctx => ctx.reply ('hi there!'))
 
 bot.command('news', async ctx => {
     
     const country = ctx.message.text.substring(6)
     const newsapiurl = 'http://newsapi.org/v2/top-headlines'
     const apikey = global.env.NEWS_API_KEY
+
+    // display the menu if no country is specified with the command
+    if (country.length<=0){
+        return menuMiddleware.replyToContext(ctx)
+    }
 
     ctx.reply(`noted that you want some news from ${country}`)
 
@@ -59,13 +65,31 @@ bot.command('news', async ctx => {
     //     results[i].publishedat = results[i].publishedat.replace('Z', ' ');
     // }
 
-    console.info(results)
+    // console.info(results)
 
     for(var i=0; i < results.length; i++) {
         ctx.reply(results[i].title + '\n\n' + results[i].summary + '\n\n' + results[i].url)
     }
 
 })
+
+// create a menu
+const menuTemplate = new MenuTemplate(ctx => `Hey ${ctx.from.first_name}!`)
+
+menuTemplate.interact('SG', 'sg', {
+    do: async ctx => ctx.answerCbQuery('SG').then(() => true)
+  })
+menuTemplate.interact('US', 'us', {
+    do: async ctx => ctx.answerCbQuery('US').then(() => true),
+    joinLastRow: true
+  })
+menuTemplate.interact('KR', 'kr', {
+    do: async ctx => ctx.answerCbQuery('KR').then(() => true),
+  })
+
+const menuMiddleware = new MenuMiddleware('/', menuTemplate)
+// // bot.c ommand('test', ctx => menuMiddleware.replyToContext(ctx))    
+bot.use(menuMiddleware)
 
 // start the bot
 bot.launch()
